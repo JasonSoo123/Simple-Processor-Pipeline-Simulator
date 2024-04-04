c844bc3527ba47c1a83d203399323b5171f46a5e
JasonSoo123 <soojason4@gmail.com>
2024-04-04 11:00:52 -0700

    updated main

--- proj.cpp
+++ proj.cpp
@@ -1,31 +1,31 @@
 #include "proj.hpp"
 using namespace std;
 
-
-int main (int argc, char* argv[])
-{
-    if (argc >= 5)
-    {
+int main(int argc, char* argv[]) {
+    if (argc >= 5) {
         string filename = argv[1];
         ifstream infile(filename);
+
+        if (!infile.is_open()) {
+            cerr << "Failed to open file: " << filename << endl;
+            return 1;
+        }
+        
         string line;
-        //read line
-        //
-        int starting_instruction = atof(argv[2]);
-        int simulating_instruction = atof(argv[3]);
-        int width = atof(argv[4]);
+        int starting_instruction = stoi(argv[2]);
+        int simulating_instruction = stoi(argv[3]);
+        int width = stoi(argv[4]);
         int cycle_count = 0;
-        while (getline(filename, line) && (cycle_count < simulating_instruction))
-        {   
+        while (getline(infile, line) && (cycle_count < simulating_instruction)) {
             
-
-
             cycle_count++;
         }
+        
+        infile.close(); 
+        
+    } else {
+        cout << "Insufficient command line arguments provided." << endl;
+    }
 
-    }
-    else
-    {
-        cout << "Insufficent command line arguments provided."<<endl;
-    }
+    return 0;
 }
