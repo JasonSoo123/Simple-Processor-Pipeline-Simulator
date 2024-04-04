2766fdc04ba03a3a341a52f1a20df3c244cb3d31
zsw <zsw@asb9838n-c14.csil.sfu.ca>
2024-04-04 11:47:34 -0700

    handled input error

--- proj.cpp
+++ proj.cpp
@@ -1,13 +1,17 @@
 #include "proj.hpp"
 using namespace std;
 
-int main(int argc, char* argv[]) {
-    if (argc >= 5) {
+
+int main(int argc, char* argv[]) 
+{
+    if (argc >= 5) 
+    {
         string filename = argv[1];
         ifstream infile(filename);
 
-        if (!infile.is_open()) {
-            cerr << "Failed to open file: " << filename << endl;
+        if (!infile.is_open()) 
+        {
+            cout << "Failed to open file: " << filename << endl;
             return 1;
         }
         
@@ -15,15 +19,25 @@ int main(int argc, char* argv[]) {
         int starting_instruction = stoi(argv[2]);
         int simulating_instruction = stoi(argv[3]);
         int width = stoi(argv[4]);
+
+        if ((width>5)||(width<0)||(starting_instruction<0)||(simulating_instruction<0))
+        {
+            cout << "input Error. Terminating Simulation...\n" << endl;
+        }
+
         int cycle_count = 0;
-        while (getline(infile, line) && (cycle_count < simulating_instruction)) {
+        struct Pipeline * pipeline = InitalizePipeline();
+        while (getline(infile, line) && (cycle_count < simulating_instruction)) 
+        {
             
             cycle_count++;
         }
         
         infile.close(); 
         
-    } else {
+    } 
+    else 
+    {
         cout << "Insufficient command line arguments provided." << endl;
     }
 
