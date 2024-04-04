a006d2fd2afd71ead24b9039250e9c1d9d5687f8
zsw <zsw@asb9838n-c14.csil.sfu.ca>
2024-04-04 10:57:27 -0700

    Updated main

--- proj.cpp
+++ proj.cpp
@@ -10,9 +10,18 @@ int main (int argc, char* argv[])
         ifstream infile(filename);
         string line;
         //read line
+        //
         int starting_instruction = atof(argv[2]);
         int simulating_instruction = atof(argv[3]);
         int width = atof(argv[4]);
+        int cycle_count = 0;
+        while (getline(filename, line) && (cycle_count < simulating_instruction))
+        {   
+            
+
+
+            cycle_count++;
+        }
 
     }
     else
